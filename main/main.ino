fe4446157cf2b8a73c6ce797d032de0e9910d824
Victor Parra <pvictor3@gmail.com>
2019-02-13 16:53:23 -0600

    Se agregó configuración de BLE y callbacks

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,48 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+BLEServer* pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+bool deviceConnected = false;
+
+class MyServerCallbacks: public BLEServerCallbacks{
+  void onConnect(BLEServer* pServer){
+      deviceConnected = true;
+    };
+
+  void onDisconnect(BLEServer* pServer){
+      deviceConnected = false;
+    }
+  };
+
+class MyCallbacks: public BLECharacteristicCallbacks{
+  void onWrite(BLECharacteristic *pCharacteristic){
+      std::string rxValue = pCharacteristic -> getValue();
+      if(rxValue.length() > 0)
+      {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+        Serial.println(rxValue);
+      }
+    }
+  };
+
+void IRAM_ATTR isr();
 const byte intPin = 15;
 volatile int pulseCounter = 0;
 
 const byte rs = 5, en = 17, d4 = 16, d5 = 4, d6 = 0, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void IRAM_ATTR isr();
+const byte voltagePin = 36;
 
 void setup() {
   Serial.begin(115200);
@@ -19,12 +55,49 @@ void setup() {
   //Inicializar LCD
   lcd.begin(16, 2);
   lcd.print("Hello world!");
+
+  //Configurar BLE
+  BLEDevice::init("ESP32");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer -> createService(SERVICE_UUID);
+  pCharacteristic = pService -> createCharacteristic(
+    CHARACTERISTIC_UUID_TX,
+    BLECharacteristic::PROPERTY_NOTIFY);
+
+  pCharacteristic -> addDescriptor(new BLE2902());
+
+  pCharacteristic = pService -> createCharacteristic(
+    CHARACTERISTIC_UUID_RX,
+    BLECharacteristic::PROPERTY_WRITE);
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  pService -> start();
+
+  pServer -> getAdvertising() -> start();
+
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // mostrar el numero de pulsos
   lcd.setCursor(0, 1);
   lcd.print(pulseCounter);
+
+  if(deviceConnected){
+    int txValue = analogRead(voltagePin);
+    char txString[8];
+    dtostrf(txValue, 1, 2, txString);
+
+    pCharacteristic -> setValue(txString);
+    pCharacteristic -> notify();
+    Serial.print("*** Sent Value: ");
+    Serial.print(txString);
+    Serial.println(" ***");
+    }
+    delay(1000);
 }
 
 void IRAM_ATTR isr(){
